ad62abc53bd81eafce70b167f2f73730de5ce0b2
jsuit <jonathan.suit@gmail.com>
2016-04-15 11:20:15 -0400

    changed name of file

--- load_Mappings.lua
+++ load_Mappings.lua
@@ -1,11 +1,11 @@
 
 tds = require 'tds'
 torch.setdefaulttensortype('torch.FloatTensor')
-local indxToVocab = torch.load("indxToVocabB.t7")
+local indxToVocab = torch.load("IndxToVocabB.t7")
 local vocatToIndx = torch.load("vocabToIndxB.t7")
 local dimension = 300
 local file = "LearnedVectorsUniform.txt"
-local h = tds.Hash()
+local h = {}
 for i=1,#indxToVocab do
   local word= indxToVocab[i]
   h[word] = torch.rand(300)
